0c0b51067a5ad7ed552d4992fd2e6836335dc4cb
Lawrence Lee <llee@eq3.ca>
2022-12-19 10:45:59 -0600

    nextjs build

--- hosting/website-fe/pages/index.tsx
+++ hosting/website-fe/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      Hello World website. lol<Link href="/about">About</Link>
+      Hello World website. lol <Link href="/about">About</Link>
     </div>
   )
 }
